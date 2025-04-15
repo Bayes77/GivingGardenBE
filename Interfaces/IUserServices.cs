7bacae89d0c566ef9f64711e949cc1e42a6d359d
Christina Vieau <cvieau@live.com>
2025-04-14 22:57:08 -0400

    modified user interfaces, endpoints, data, repository, and services

--- Interfaces/IUserServices.cs
+++ Interfaces/IUserServices.cs
@@ -1,6 +1,15 @@
-﻿namespace GivingGardenBE.Interfaces
+﻿using GivingGardenBE.Models;
+
+namespace GivingGardenBE.Interfaces
 {
     public interface IUserServices
     {
+        // responsible for processing business logic
+        // will call the repository layer to do the actual CRUD operations
+        Task<List<User>> GetUserAsync();
+        Task<User> GetUserByIdAsync(int id);
+        Task<User> CreateUserAsync(User user);
+        Task<User> UpdateUserAsync(int id, User user);
+        Task<User> DeleteUserAsync(int id);
     }
 }
--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,6 +1,60 @@
-﻿namespace GivingGardenBE.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using GivingGardenBE.Data;
+using GivingGardenBE.Interfaces;
+using GivingGardenBE.Models;
+
+namespace GivingGardenBE.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
+
     {
+        private readonly GivingGardenBEDbContext _context;
+
+        public UserRepository(GivingGardenBEDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<User>> GetUserAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> CreateUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> UpdateUserAsync(int id, User user)
+        {
+            var existingUser = await _context.Users.FindAsync(id);
+            if (existingUser == null)
+            {
+                return null;
+            }
+            existingUser.Name = user.Name;
+            existingUser.Email = user.Email;
+            existingUser.Image = user.Image;
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> DeleteUserAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return null;
+            }
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
     }
 }
