6bc1bd312a130e0d2278a9f3ad29fb7b6bad49bc
Bayez77 <mrbayez88@gmail.com>
2025-04-17 19:57:17 -0500

    merge issues

--- Data/GivingGardenBEDbContext.cs
+++ Data/GivingGardenBEDbContext.cs
@@ -1,18 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;    
-﻿using Microsoft.EntityFrameworkCore;    
+﻿
+using Microsoft.EntityFrameworkCore;
 using GivingGardenBE.Models;
-using GivingGardenBE.Data;
-using GivingGardenBE.Data;
-using GivingGardenBE.Data;
-using GivingGardenBE.Data;
 
 
 
-=======
-﻿using Microsoft.EntityFrameworkCore;
-using GivingGardenBE.Models;
-using GivingGardenBE.Data;
->>>>>>> main
 namespace GivingGardenBE.Data
 {
     public class GivingGardenBEDbContext : DbContext
@@ -20,12 +11,14 @@ namespace GivingGardenBE.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Subscription> Subscriptions { get; set; }
         public DbSet<Organization> Organizations { get; set; }
-        public DbSet<PaymentTypes> PaymentTypes { get; set; }
+
+
         public GivingGardenBEDbContext(DbContextOptions<GivingGardenBEDbContext> options)
             : base(options)
         {
         }
->>>>>>> main
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().HasData(UserData.Users);
